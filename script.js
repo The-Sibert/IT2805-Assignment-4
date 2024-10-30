efd70be3e9e009115f1b8cd1bcb4cf7b8b883536
sivertvaarum <sivertlv@stud.ntnu.no>
2024-10-30 15:14:16 +0100

    gjort til og med oppgave 1.5

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
 /* Task  1.2 */
 console.log('Task  1.2')
-
-/* Task  1.3 */
+for (i = 1; i <= 20; i++)
+    {
+        console.log(i)
+    }
+// Task  1.3
 console.log('Task  1.3')
-
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
+for(i = 0; i < numbers.length; i++)
+{
+    if(numbers[i] % 3 == 0 && numbers[i] % 5 == 0){
+        console.log("eplekake")
+    }
+    else if(numbers[i] % 3 == 0){
+         console.log("eple")   
+    }
+    else if(numbers[i] % 5 == 0){
+        console.log("kake")
+    }
+    else{
+        console.log(numbers[i])
+    }       
+}
 /* Task  1.4 */
-
+let title = document.getElementById("title")
+title.innerText = "Hello Javascript"
 /* Task  1.5 */
-function changeDisplay () {
+let btns = document.getElementsByClassName("buttons")[0]
+let displayBtn = btns.firstElementChild
+let visibilityBtn = btns.children[1]
+let resetBtn = btns.lastElementChild
+let magic = document.getElementById("magic")
+displayBtn.addEventListener("click", changeDisplay)
+visibilityBtn.addEventListener("click", changeVisibility)
+resetBtn.addEventListener("click", reset)
 
+function changeDisplay(e){
+    
+    magic.style.display = "none"
 }
-
 function changeVisibility () {
-
+    magic.style.visibility = "hidden"
+    magic.style.display = "block"
 }
-
-function reset () {
-
+function reset(){
+    magic.style = null
 }
-
 /* Task 1.6 */
 const technologies = [
     'HTML5',
